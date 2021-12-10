12d739c4b1f471b3b7897a6463abc4f49b26f3e2
Hyunsang-coder <joohs86@naver.com>
2021-12-11 00:14:12 +0900

    Update Dec 11
    12:13PM
    Created Patrol Path prefab

---
+++ Assets/Script/Controller/PatrolPath.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            const float wayPointGizmoRadius = 0.3f;
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), wayPointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+            }
+        }
+
+        private int GetNextIndex(int i)
+        {
+            if (transform.childCount == i + 1)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+}
+
